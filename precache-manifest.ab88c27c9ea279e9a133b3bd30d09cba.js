dd90f4657dfc83cdd2ee362f0e71ab032b59e6eb
Julie Nginn <nginn.julie@gmail.com>
2020-04-27 17:20:50 +0200

    Updates

---
+++ precache-manifest.ab88c27c9ea279e9a133b3bd30d09cba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5ecf423523acae7b784cb606cc7f94",
+    "url": "/the-quiz-master/index.html"
+  },
+  {
+    "revision": "d3ee0b6c2ca7749d0ab7",
+    "url": "/the-quiz-master/static/js/2.6bb5742c.chunk.js"
+  },
+  {
+    "revision": "cfcdebbd5c361eb80dad4bc286f1bc4f",
+    "url": "/the-quiz-master/static/js/2.6bb5742c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2da8cb91c56827d2707a",
+    "url": "/the-quiz-master/static/js/main.f552b79a.chunk.js"
+  },
+  {
+    "revision": "29379f764442de28410a",
+    "url": "/the-quiz-master/static/js/runtime-main.e1dc3225.js"
+  },
+  {
+    "revision": "9caf24c44715cb274dca1142136a56b4",
+    "url": "/the-quiz-master/static/media/applause.9caf24c4.wav"
+  },
+  {
+    "revision": "a1a20659a664a1a931814045935eee90",
+    "url": "/the-quiz-master/static/media/bg.a1a20659.jpg"
+  },
+  {
+    "revision": "89195f363867441e67b69c6e4930316c",
+    "url": "/the-quiz-master/static/media/clock.89195f36.ogg"
+  },
+  {
+    "revision": "b3e755d07de182199ed9842c94734e78",
+    "url": "/the-quiz-master/static/media/crown.b3e755d0.png"
+  },
+  {
+    "revision": "85f9d2993df5d4580fd7df4c6a48440d",
+    "url": "/the-quiz-master/static/media/your_turn.85f9d299.mp3"
+  }
+]);
